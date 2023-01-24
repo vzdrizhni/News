7a9652fbb57119e4dd76364381d3d8126748836a
Roman <vzdrizhni@gmail.com>
2023-01-24 16:44:41 +0500

    Add post creation with picture attachment

---
+++ app/models/post.rb
@@ -0,0 +1,4 @@
+class Post < ApplicationRecord
+  include ImageUploader::Attachment(:image)
+  validates :title, presence: true
+end
---
+++ db/migrate/20230124105716_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :body
+      t.date :date
+      t.text :image_data
+
+      t.timestamps
+    end
+  end
+end
