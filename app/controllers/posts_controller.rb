a775022285168e2f3b2f56e16b66fcf2c72a8ab0
Roman <vzdrizhni@gmail.com>
2023-01-24 19:18:26 +0500

    Set default page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
 
+  def main
+    @last_posts = Post.first(3)
+  end
+
   # GET /posts or /posts.json
   def index
     @posts = Post.order(:date).page params[:page]
