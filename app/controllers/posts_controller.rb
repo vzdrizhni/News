2af677122c1a390dbe23e8ee46516bf3128e8e2a
Roman <vzdrizhni@gmail.com>
2023-01-25 16:21:03 +0500

    Save date for post model before save

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :body, :date, :image)
+      params.require(:post).permit(:title, :body, :date, :image, :body)
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ApplicationRecord
   include ImageUploader::Attachment(:image)
   validates :title, presence: true
+  validates :body, presence: true
+  has_rich_text :body
+
+  before_save do
+    self.date = Date.today
+  end
 end
